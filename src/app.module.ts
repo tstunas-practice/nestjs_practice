e01d65f7c97caf773635ce361bf26c441d2c881f
Park Jin <tstunas@gmail.com>
2023-02-16 10:41:40 +0900

    Feat: ConfigModule 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
+import { TypeOrmConfigService } from './config/typeorm.config.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'sparta',
-      database: 'board',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Production 환경에서는 false
+    ConfigModule.forRoot({ isGlobal: true }), // 일단 이것은 무조건 가장 위에서!
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // 주목
+      useClass: TypeOrmConfigService,
+      inject: [ConfigService], // 주목
     }),
     BoardModule,
   ],
