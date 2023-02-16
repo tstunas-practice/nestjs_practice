bad73e5d74023363008ae4b202abe4a6f098fa9e
Park Jin <tstunas@gmail.com>
2023-02-16 10:29:00 +0900

    Feat: TypeOrmModule.forRoot()

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [BoardModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'sparta',
+      database: 'board',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Production 환경에서는 false
+    }),
+    BoardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
